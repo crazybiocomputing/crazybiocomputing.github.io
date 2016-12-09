1484676c79c65c0d22269472554cf0bdbbfb40f1
jeesay <jjeesay@gmail.com>
2016-12-09 18:12:26 +0100

    Update debrief.js

--- javascripts/debrief.js
+++ javascripts/debrief.js
@@ -2,9 +2,9 @@ function debrief(v,org,target) {
     localStorage.setItem('crazybio_home',window.location);
 }
 
-function configButton(elt) {
+function configButton(elt, backcolor) {
     elt.href = "https://moodle1.u-bordeaux.fr/mod/page/view.php" + "?"+"id="+elt.dataset.href;
-    elt.style.background = "#0078e7"; 
+    elt.style.background = backcolor; 
     elt.style.color = "white";
     // elt.style.border = "1px solid black";
     elt.style.borderRadius = "2px"; 
@@ -18,6 +18,28 @@ function configButtons() {
     let buttons = document.querySelectorAll('.crazybutton');
     for (let i in buttons) {
         let elt = buttons[i];
-        configButton(elt);
+        configButton(elt,"#0078e7");
     }
+}
+
+function debrief_moderate_menubar() {
+    let home = localStorage.getItem('crazybio_home');
+    let output = document.getElementById("output");
+    let remind = output.createElement('a'); 
+    remind.innerHTML='<a href="'+home+'">Reminders</a>';
+    configButton(remind,'green');
+    let challenges = output.createElement('a'); 
+    challenges.innerHTML='<a href="'+home+'">Challenges</a>';
+    configButton(challenges,'orange');
+    let homelink = output.createElement('a'); 
+    homelink.innerHTML='<a href="'+home+'">Return to Page</a>';
+    configButton(homelink,"#0078e7");
+}
+
+function debrief_very_easy_menubar() {
+    let home = localStorage.getItem('crazybio_home');
+    let output = document.getElementById("output");
+    output.innerHTML='<a href="'+home+'">Reminders</a>';
+    output.innerHTML='<a href="'+home+'">Challenges</a>';
+    output.innerHTML='<a href="'+home+'">Return to Page</a>';
 }
