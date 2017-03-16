c432bae1d467ff9e311468c9eb2110dd56d12d0b
jeesay <jjeesay@gmail.com>
2017-03-16 14:23:13 +0100

    Update crazy_header.js

--- minigames/js/crazy_header.js
+++ minigames/js/crazy_header.js
@@ -1,12 +1,19 @@
 let menu = document.querySelector('header');
-let minigame = 
+let url = window.location.href.substr(window.location.href.lastIndexOf('bioinfo/') );
+var index = 0;
+while (minigames[index] !== url) {
+  index++;
+}
+let level = minigames[index].level;
+let gameIndex = minigames[index].index;
 let html = `
 <ul>
-<li><a href="../index.php">[ H o m e ]</a></li>
+<li><a href="../index.html">[ H o m e ]</a></li>
 <li><a href="help.html">[ H e l p ]</a></li>
 <li><a href="history.html">&mdash; c&nbsp;&nbsp;r&nbsp;&nbsp;a&nbsp;&nbsp;z&nbsp;&nbsp;y
 &nbsp;&nbsp;b&nbsp;&nbsp;i&nbsp;&nbsp;o
 &nbsp;&nbsp;c&nbsp;&nbsp;o&nbsp;&nbsp;m&nbsp;&nbsp;p&nbsp;&nbsp;u&nbsp;&nbsp;t&nbsp;&nbsp;i&nbsp;&nbsp;n&nbsp;&nbsp;g  &mdash;</a></li>
-<li><a href="#">[ L e v e l # <?php echo $level;?> &mdash; g a m e # <?php echo $gameIndex;?> ]</a></li>
+<li><a href="#">[ L e v e l # ` + level+ '&mdash; g a m e # '+ gameIndex +` ]</a></li>
 </ul>
-</div>
+</div>`;
+menu.html = html;
