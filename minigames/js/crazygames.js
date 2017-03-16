71ecd191752f59cdd35a0de4790092be9f9711cb
jeesay <jjeesay@gmail.com>
2017-03-16 17:02:21 +0100

    Update crazygames.js

--- minigames/js/crazygames.js
+++ minigames/js/crazygames.js
@@ -13,6 +13,38 @@ for (let i=1; i < titles.length; i++) {
 }
 output.innerHTML = html;
  
+function create_header() {
+    let menu = document.querySelector('header');
+    let family = 'bioinfo'; // Must be checked between ip, bioinfo, and tutorial
+    let url = window.location.href.substr(window.location.href.lastIndexOf(family + '/') + family.length + 1);
+    console.log(url);
+    var index = 0;
+    while (minigames[index].url !== url && index < minigames.length) {
+        index++;
+    }
+    let level = minigames[index].level;
+    let gameIndex = minigames[index].index;
+    localStorage.setItem('crazybio_accession', index);
+    let html = `
+<ul>
+<li><a href="../index.html">[ H o m e ]</a></li>
+<li><a href="help.html">[ H e l p ]</a></li>
+<li><a href="history.html">&mdash; c&nbsp;&nbsp;r&nbsp;&nbsp;a&nbsp;&nbsp;z&nbsp;&nbsp;y
+&nbsp;&nbsp;b&nbsp;&nbsp;i&nbsp;&nbsp;o
+&nbsp;&nbsp;c&nbsp;&nbsp;o&nbsp;&nbsp;m&nbsp;&nbsp;p&nbsp;&nbsp;u&nbsp;&nbsp;t&nbsp;&nbsp;i&nbsp;&nbsp;n&nbsp;&nbsp;g  &mdash;</a></li>
+<li><a href="#">[ L e v e l # ` + level+ '&mdash; g a m e # '+ gameIndex +` ]</a></li>
+</ul>
+</div>`;
+    console.log(html);
+    menu.innerHTML = html;
+    
+    // Create Title
+    let elt = document.createElement('title');
+    elt.textContent = 'Level #'+level+' Game #'+gameIndex + ' - CrazyBioComputing';
+    document.head.appendChild(elt);
+}
+
+
 function next_game() {
   console.log(localStorage);
   let index = localStorage.crazybio_accession;
