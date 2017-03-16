dc44adca15dc72b81de193574a80972c6455e4a0
jeesay <jjeesay@gmail.com>
2017-03-16 14:32:26 +0100

    Update crazy_header.js

--- minigames/js/crazy_header.js
+++ minigames/js/crazy_header.js
@@ -1,5 +1,5 @@
 let menu = document.querySelector('header');
-let url = window.location.href.substr(window.location.href.lastIndexOf('bioinfo/') + 8);
+let url = window.location.href.substr(window.location.href.lastIndexOf('bioinfo/') + 7);
 console.log(url);
 var index = 0;
 while (minigames[index] !== url && index < minigames.length) {
