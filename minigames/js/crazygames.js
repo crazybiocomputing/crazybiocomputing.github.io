a6e134fcbd5d433c06f2b9025f2174d8e9a72c8b
jeesay <jjeesay@gmail.com>
2017-03-16 12:22:20 +0100

    Update crazygames.js

--- minigames/js/crazygames.js
+++ minigames/js/crazygames.js
@@ -5,7 +5,7 @@ for (let i=0; i < titles.length; i++) {
   html +='<table><tr>';
   for (let j = 0; j < minigames.length; j++) {
     if (minigames[j].level === i) {       
-      html +='<td><a href="'+minigames[i].url+'"><img src="../img/thumbnail_unknown.png"></a></td>';
+      html +='<td><a href="'+minigames[j].url+'"><img src="../img/thumbnail_unknown.png"></a></td>';
     }
 
   }
