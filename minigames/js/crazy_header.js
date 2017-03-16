42bf43e4b4a4c9c10bd2afd570c1871778f00658
jeesay <jjeesay@gmail.com>
2017-03-16 14:17:17 +0100

    Create crazy_header.js

---
+++ minigames/js/crazy_header.js
@@ -0,0 +1,12 @@
+let menu = document.querySelector('header');
+let minigame = 
+let html = `
+<ul>
+<li><a href="../index.php">[ H o m e ]</a></li>
+<li><a href="help.html">[ H e l p ]</a></li>
+<li><a href="history.html">&mdash; c&nbsp;&nbsp;r&nbsp;&nbsp;a&nbsp;&nbsp;z&nbsp;&nbsp;y
+&nbsp;&nbsp;b&nbsp;&nbsp;i&nbsp;&nbsp;o
+&nbsp;&nbsp;c&nbsp;&nbsp;o&nbsp;&nbsp;m&nbsp;&nbsp;p&nbsp;&nbsp;u&nbsp;&nbsp;t&nbsp;&nbsp;i&nbsp;&nbsp;n&nbsp;&nbsp;g  &mdash;</a></li>
+<li><a href="#">[ L e v e l # <?php echo $level;?> &mdash; g a m e # <?php echo $gameIndex;?> ]</a></li>
+</ul>
+</div>
