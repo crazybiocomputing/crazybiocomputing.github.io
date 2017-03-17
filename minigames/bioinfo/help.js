9b9de0a6418fd2a43b515dfa33989c07a2d266ea
jeesay <jjeesay@gmail.com>
2017-03-17 14:03:46 +0100

    Create help.js

---
+++ minigames/bioinfo/help.js
@@ -0,0 +1,12 @@
+var help = {
+"10": {
+  "ID": 10,
+  "msg": [
+    "<br />",
+    "<p>Click on <span style="padding:10px;text-decoration: none;text-transform: uppercase;font-family: Arial, Helvetica, sans-serif;font-size: 14px;font-weight: normal;color: #FFFFFF;background-color:black;">[ L e v e l # $level$ &mdash; g a m e # $game$ ]</span> to return to the mini-game </p>",
+    "<br /><br />",
+    "<p style="font-size: 150%">The clue is in the URL &#9786;</p>",
+    "<p style="font-size: 150%">... and don't forget to add the extension <i>'.html'</i> &#9786;</p>"
+    ]
+ }
+  ];
