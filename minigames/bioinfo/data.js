09eae12b0809172a6188008b7d718c069d22de30
jeesay <jjeesay@gmail.com>
2017-03-17 14:09:04 +0100

    Update data.js

--- minigames/bioinfo/data.js
+++ minigames/bioinfo/data.js
@@ -8,7 +8,20 @@ var minigames = [
     "symbol": "a", 
     "symbol2": "b", 
     "url": "whereis/fasta.html", 
-    "url2": "whereis/turtle.html"
+    "url2": "whereis/turtle.html",
+    "msg": [
+    "<br />",
+    "<p>Click on <span style=\"padding:10px;text-decoration: none;text-transform: uppercase;font-family: Arial, Helvetica, sans-serif;font-size: 14px;font-weight: normal;color: #FFFFFF;background-color:black;\">[ L e v e l # $level$ &mdash; g a m e # $game$ ]</span> to return to the mini-game </p>",
+    "<br /><br />",
+    "<p style=\"font-size: 150%\">The clue is in the URL &#9786;</p>",
+    "<p style=\"font-size: 150%\">... and don't forget to add the extension <i>'.html'</i> &#9786;</p>"
+    ],
+    "history": [
+      "2012/01/23: Help added",
+      "2012/01/23: History created",
+      "2012/01/23: Mini-game created",
+      "2017/03/17: Refactoring with JS"
+    ]
   }, 
   {
     "ID": 11, 
